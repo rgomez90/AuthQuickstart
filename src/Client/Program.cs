4c62dd2d3d0c6c93de0a2c7b1d8a860fd91ffeef
Rafael Gomez <Rafael.Diaz@dccs.at>
2022-11-20 23:37:06 +0100

    First commit

---
+++ src/Client/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Text.Json;
+using IdentityModel.Client;
+
+var client = new HttpClient();
+// discover endpoints from metadata
+
+var disco = await client.GetDiscoveryDocumentAsync("https://localhost:5001");
+if (disco.IsError)
+{
+    Console.WriteLine(disco.Error);
+    return;
+}
+
+// request token
+var tokenResponse = await client.RequestClientCredentialsTokenAsync(new ClientCredentialsTokenRequest
+{
+    Address = disco.TokenEndpoint,
+    ClientId = "client",
+    ClientSecret = "secret",
+    Scope = "users"
+});
+
+if (tokenResponse.IsError)
+{
+    Console.WriteLine(tokenResponse.Error);
+    return;
+}
+
+Console.WriteLine(tokenResponse.Json);
+
+// call api
+var apiClient = new HttpClient();
+apiClient.SetBearerToken(tokenResponse.AccessToken);
+
+var response = await apiClient.GetAsync("https://localhost:6001/identity");
+if (!response.IsSuccessStatusCode)
+{
+    Console.WriteLine(response.StatusCode);
+}
+else
+{
+    var content = await response.Content.ReadAsStringAsync();
+    Console.WriteLine(JsonSerializer.Serialize(JsonDocument.Parse(content), new JsonSerializerOptions() { WriteIndented = true }));
+}
